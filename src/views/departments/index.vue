89aec8510ddd9a926a15071a0b6fcc3119762490
lcruan <1416802331@qq.com>
2022-09-22 22:21:37 +0800

    组织架构tree

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -3,28 +3,91 @@
     <div class="app-container">
       <!-- 组织架构内容 头部-->
       <!-- <el-card>就是div -->
-      <el-dard>
+      <el-card class="tree-card">
         <!-- 放置结构内容 -->
-        <el-row>
+        <el-row type="flex" justify="space-between" align="middle" style="height:40px;">
           <el-col>
             <!-- 左侧内容 -->
             <span>江苏传智播客教育科技股份有限公司</span>
           </el-col>
-          <el-row>
+          <el-col :span="4">
             <!-- 右侧内容 -->
-          </el-row>
+            <el-row type="flex" justify="end">
+              <el-col>负责人</el-col>
+              <el-col>
+                <!-- 下拉菜单 -->
+                <el-dropdown>
+                  <!-- 内容 -->
+                  <span>操作
+                    <i class="el-icon-arrow-down" />
+                  </span>
+                  <el-dropdown-menu slot="dropdown">
+                    <!-- 下拉选项 -->
+                    <el-dropdown-item>添加子部门</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </el-col>
         </el-row>
-      </el-dard>
+        <!-- 放置一个el-tree -->
+        <el-tree :data="list" :props="defaultProps" :default-expand-all="true" />
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      list: [{
+        label: '一级 1',
+        children: [{
+          label: '二级 1-1',
+          children: [{
+            label: '三级 1-1-1'
+          }]
+        }]
+      }, {
+        label: '一级 2',
+        children: [{
+          label: '二级 2-1',
+          children: [{
+            label: '三级 2-1-1'
+          }]
+        }, {
+          label: '二级 2-2',
+          children: [{
+            label: '三级 2-2-1'
+          }]
+        }]
+      }, {
+        label: '一级 3',
+        children: [{
+          label: '二级 3-1',
+          children: [{
+            label: '三级 3-1-1'
+          }]
+        }, {
+          label: '二级 3-2',
+          children: [{
+            label: '三级 3-2-1'
+          }]
+        }]
+      }],
+      defaultProps: {
+        label: 'label',
+        children: 'children'
+      }
+    }
+  }
 }
 </script>
 
-  <style>
-
+  <style scoped>
+.tree-card {
+  padding: 30px  140px;
+  font-size:14px;
+}
   </style>
