9a58fb0ee2956f0eec8dd7184394dda303af6ae8
lcruan <1416802331@qq.com>
2022-09-23 22:11:58 +0800

    实现树形的静态组织结构

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -31,7 +31,34 @@
           </el-col>
         </el-row>
         <!-- 放置一个el-tree -->
-        <el-tree :data="list" :props="defaultProps" :default-expand-all="true" />
+        <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
+          <!-- 传入内容 插槽内容会循环多次 有多少节点就循环多少次 -->
+          <el-row type="flex" justify="space-between" align="middle" style="height:40px;width: 100%;">
+            <el-col>
+              <!-- 左侧内容 -->
+              <span>江苏传智播客教育科技股份有限公司</span>
+            </el-col>
+            <el-col :span="4">
+              <!-- 右侧内容 -->
+              <el-row type="flex" justify="end">
+                <el-col>负责人</el-col>
+                <el-col>
+                  <!-- 下拉菜单 -->
+                  <el-dropdown>
+                    <!-- 内容 -->
+                    <span>操作
+                      <i class="el-icon-arrow-down" />
+                    </span>
+                    <el-dropdown-menu slot="dropdown">
+                      <!-- 下拉选项 -->
+                      <el-dropdown-item>添加子部门</el-dropdown-item>
+                    </el-dropdown-menu>
+                  </el-dropdown>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </el-tree>
       </el-card>
     </div>
   </div>
@@ -41,43 +68,11 @@
 export default {
   data() {
     return {
-      list: [{
-        label: '一级 1',
-        children: [{
-          label: '二级 1-1',
-          children: [{
-            label: '三级 1-1-1'
-          }]
-        }]
-      }, {
-        label: '一级 2',
-        children: [{
-          label: '二级 2-1',
-          children: [{
-            label: '三级 2-1-1'
-          }]
-        }, {
-          label: '二级 2-2',
-          children: [{
-            label: '三级 2-2-1'
-          }]
-        }]
-      }, {
-        label: '一级 3',
-        children: [{
-          label: '二级 3-1',
-          children: [{
-            label: '三级 3-1-1'
-          }]
-        }, {
-          label: '二级 3-2',
-          children: [{
-            label: '三级 3-2-1'
-          }]
-        }]
-      }],
+      departs: [
+        { name: '总裁办', children: [{ name: '董事会' }] },
+        { name: '行政部' }, { name: '人事部' }],
       defaultProps: {
-        label: 'label',
+        label: 'name',
         children: 'children'
       }
     }
