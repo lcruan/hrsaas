4466e80e863bc055d74bff8a2211ae4340b445aa
lcruan <1416802331@qq.com>
2022-09-27 20:52:48 +0800

    excel导出基本机构

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -10,7 +10,7 @@
         <!-- 右侧显示按钮 excel导入 excel导出 新增员工 -->
         <template v-slot:after>
           <el-button size="small" type="success" @click="$router.push('/import')">excel导入</el-button>
-          <el-button size="small" type="danger">excel导出</el-button>
+          <el-button size="small" type="danger" @click="exportData">excel导出</el-button>
           <el-button size="small" type="primary" @click="showDialog = true">新增员工</el-button>
         </template>
       </page-tools>
@@ -112,6 +112,53 @@ export default {
       } catch (error) {
         console.log(error)
       }
+    },
+    exportData() {
+      const headers = {
+        '手机号': 'mobile',
+        '姓名': 'username',
+        '入职日期': 'timeOfEntry',
+        '聘用形式': 'formOfEmployment',
+        '转正日期': 'correctionTime',
+        '工号': 'workNumber',
+        '部门': 'departmentName'
+      }
+      // 导出excel
+      import('@/vendor/Export2Excel').then(async excel => {
+        // excel是引入文件的导出对象
+        // 导出 header从哪里来 --Object.keys(headers)
+        // data从哪里来
+        // 现在没有一个接口获取所有数据
+        // 获取员工接口 页码 每页条数 100
+        const { rows } = await getEmployeeList({ page: 1, size: this.page.total })
+        const data = this.formatJson(headers, rows) // 返回的data就是导出的结构
+        excel.export_json_to_excel({
+          header: Object.keys(headers),
+          data
+        })
+
+        // excel.export_json_to_excel({
+        //   header: ['姓名', '工资'],
+        //   data: [['张三', '3000'], ['李四', '5000']],
+        //   filename: '员工工资表'
+        // })
+        // [{username:'张三'},{mobile:13323412312}] => [[]]
+        // 转换 数据结构 还要和表头的顺序对应上
+        // 要求转出的标题是中文
+      })
+    },
+    // 将表头数据和数据进行对应
+    // [{}] ->转换为 [[]]
+    formatJson(headers, rows) {
+      return rows.map(item => {
+        // item是一个对象 {mobile: 131222}
+        // Object.keys(headers) ['手机号', '姓名', '入职日期']
+        return Object.keys(headers).map(key => {
+          // headers[key] 是所对应的英文名 -> mobile
+          return item[headers[key]] // ['132','张三','','']
+        })
+      })
+      // return rows.map(item => Object.keys(headers).map(key => item[headers[key]]))
     }
   }
 }
