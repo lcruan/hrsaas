3be5404249912cb181c901da59a2e4fa14bbb289
lcruan <1416802331@qq.com>
2022-10-02 08:15:34 +0800

    保存时处理头像和证件照

--- src/views/employees/components/user-info.vue
+++ src/views/employees/components/user-info.vue
@@ -89,7 +89,7 @@
         <!-- 员工照片 -->
         <el-form-item label="员工照片">
           <!-- 放置上传图片 -->
-          <image-upload :ref="myStaffPhoto" />
+          <image-upload ref="myStaffPhoto" />
         </el-form-item>
         <el-form-item label="国家/地区">
           <el-select v-model="formData.nationalArea" class="inputW2">
@@ -365,7 +365,7 @@ export default {
   methods: {
     async getUserDetailById() {
       this.userInfo = await getUserDetailById(this.userId)
-      if (this.userInfo.staffPhoto) {
+      if (this.userInfo.staffPhoto && this.userInfo.staffPhoto.trim()) {
         // 有值就表示 已经有了上传成功的图片
         // 上传成功的图片 upload: true 表示该照片已经上传成功 因为不修改图片 只是修改其他字段情境下
         this.$refs.staffPhoto.fileList = [{ url: this.userInfo.staffPhoto, upload: true }]
@@ -373,16 +373,31 @@ export default {
     },
     async getPersonalDetail() {
       this.formData = await getPersonalDetail(this.userId)
-      if (this.formData.staffPhoto) {
+      if (this.formData.staffPhoto && this.formData.staffPhoto.trim()) {
         this.$refs.myStaffPhoto.fileList = [{ url: this.formData.staffPhoto, upload: true }]
       }
     },
     async saveUser() {
-      await saveUserDetailById(this.userInfo)
+      // 先去获取头像中的地址
+      const fileList = this.$refs.staffPhoto.fileList
+      // 应该做一个判断 判断当前的图片有没有上传完成
+      if (fileList.some(item => !item.upload)) {
+        // 说明此时有图片还没有上传完成
+        this.$message.warning('此时还有图片没有上传完成')
+        return
+      }
+      // staffPhoto 由于接口问题 必须给有空格的字符串 才能存进去
+      await saveUserDetailById({ ...this.userInfo, staffPhoto: fileList.length ? fileList[0].url : ' ' })
       this.$message.success('保存用户基本信息成功')
     },
     async savePersonal() {
-      await updatePersonal(this.formData)
+      const fileList = this.$refs.myStaffPhoto.fileList
+      if (fileList.some(item => !item.upload)) {
+        // 说明此时有图片还没有上传完成
+        this.$message.warning('此时还有图片没有上传完成')
+        return
+      }
+      await updatePersonal({ ...this.formData, staffPhoto: fileList.length ? fileList[0].url : ' ' })
       this.$message.success('保存用户基础信息成功')
     }
   }
