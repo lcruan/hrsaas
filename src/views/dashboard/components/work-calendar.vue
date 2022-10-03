9b04c65beecfde9062d6e4bbd0ebf69d03d15b6c
lcruan <1416802331@qq.com>
2022-10-03 09:12:04 +0800

    实现工作日期逻辑

--- src/views/dashboard/components/work-calendar.vue
+++ src/views/dashboard/components/work-calendar.vue
@@ -4,31 +4,48 @@
     <!-- 年和月 -->
     <el-row type="flex" justify="end">
       <!-- 年 用组件给定一个日期，通过日期获取年，取前5年+后5年 -->
-      <el-select v-model="currentYear" size="small" style="width:120px;">
+      <el-select v-model="currentYear" size="small" style="width:120px;" @change="dataChange">
         <el-option v-for="item in yearList" :key="item" :label="item" :value="item" />
       </el-select>
-      <el-select v-model="currentMonth" size="small" style="width:120px;margin-left: 10px;">
+      <el-select v-model="currentMonth" size="small" style="width:120px;margin-left: 10px;" @change="dataChange">
         <!-- 循环 -->
         <el-option v-for="item in 12" :key="item" :label="item" :value="item" />
       </el-select>
     </el-row>
     <!-- 放置日历组件 -->
-    <el-calendar />
+    <el-calendar v-model="currentDate">
+      <!-- <template slot="dateCell"> -->
+      <!-- date是当前单元格的日期， data是对象 对象里有要显示的day -->
+      <template v-slot:dateCell="{date, data}">
+        <div class="date-content">
+          <span class="text"> {{ data.day | getDay }}</span>
+          <span v-if="isWeek(date)" class="rest">休</span>
+        </div>
+      </template>
+    </el-calendar>
   </div>
 </template>
 <script>
 export default {
+  filters: {
+    getDay(value) {
+      const day = value.split('-')[2]
+      return day.startsWith('0') ? day.substr(1) : day
+    }
+  },
   props: {
     startDate: {
       type: Date,
-      default: new Date() // 如果没有传递startDate 就取当前时间
+      // 回调函数返回值
+      default: () => new Date() // 如果没有传递startDate 就取当前时间
     }
   },
   data() {
     return {
       yearList: [], // 要遍历的年的数组
       currentYear: null, // 当前年份
-      currentMonth: null // 当前月份
+      currentMonth: null, // 当前月份
+      currentDate: null // 当前时间
     }
   },
   created() {
@@ -37,6 +54,18 @@ export default {
     this.currentMonth = this.startDate.getMonth() + 1
     // 快速生成数组的方法
     this.yearList = Array.from(Array(11), (v, i) => this.currentYear + i - 5)
+    // 钩子函数执行完成之后
+    this.dataChange() // 主动调用方法
+  },
+  methods: {
+    dataChange() {
+      // 生成新的日期
+      this.currentDate = new Date(`${this.currentYear}-${this.currentMonth}-1`) // 以1号为开始
+    },
+    // 判断当前是否是周末
+    isWeek(date) {
+      return date.getDay() === 6 || date.getDay() === 0
+    }
   }
 }
 </script>
